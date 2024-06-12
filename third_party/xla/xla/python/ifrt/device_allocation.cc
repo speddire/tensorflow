f6a0fdd59f7dcabc39adeaf6e7ba683ecca93c71
Hyeontaek Lim <hyeontaek@google.com>
2024-06-12 16:53:17 -0700

    [IFRT] Introduce Client::AllocateDevices() and DeviceAllocation
    `xla::ifrt::Client::AllocateDevices()` is a new API that processes a user
    request for getting an ordered set of devices that satisfies constraints
    specified in the request. It returns a new runtime object `DeviceAllocation`,
    which represents the allocated devices as a collection, which will be used
    where `DeviceList` is used today.
    
    Main use cases of the new API will include:
    
    * The user can get a set of devices in a platform-specific manner (e.g., TPU
    user code typically uses a logical "mesh" that consists of devices ordered in
    a certain way to make collectives run efficiently).
    
    * The user can get a set of devices in relation to other set of devices
    (finding CPU devices colocated with accelerator devices).
    
    * The user can request allocating a new set of devices that do not overlap with
    in-use device to run computations in parallel.
    
    This initial change implements the interface of the API and object type, and a
    simple implementation `BasicDeviceAllocation` that wraps around a `DeviceList`
    to help transition from `DeviceList` to `DeviceAllocation` in the future.
    
    PiperOrigin-RevId: 642783182

--- third_party/xla/xla/python/ifrt/device_allocation.cc
+++ third_party/xla/xla/python/ifrt/device_allocation.cc
@@ -1,4 +1,4 @@
-/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2024 The OpenXLA Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,23 +12,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#ifndef TENSORFLOW_LITE_STDERR_REPORTER_H_
-#define TENSORFLOW_LITE_STDERR_REPORTER_H_
 
-#include <cstdarg>
+#include "xla/python/ifrt/device_allocation.h"
 
-#include "tensorflow/lite/core/api/error_reporter.h"
+namespace xla {
+namespace ifrt {
 
-namespace tflite {
+char DeviceAllocation::ID = 0;
 
-// An error reporter that simply writes the message to stderr.
-struct StderrReporter : public ErrorReporter {
-  int Report(const char* format, va_list args) override;
-};
-
-// Return the default error reporter (output to stderr).
-ErrorReporter* DefaultErrorReporter();
-
-}  // namespace tflite
-
-#endif  // TENSORFLOW_LITE_STDERR_REPORTER_H_
+}  // namespace ifrt
+}  // namespace xla
